e1799deca9fcede628e076ae088a62153d2a4f7e
Augusto <augustopmello@gmail.com>
2026-03-09 14:20:48 +0000

    max

--- aula8/max.asm
+++ aula8/max.asm
@@ -8,34 +8,26 @@ I K /0001
 
 @ =0
 GD /000; carrega x0
+SB BASE
 MM MAX; salva max 0
-GD /000
 
 
 LOOP LV /005
-SUB I
+SB I
 JZ IMPRIME; 5-i = 0?
+GD /000
 GD /000; carrega x
 SB BASE
 SB MAX
 JN SKIP ; x - max < 0 ?
 AD MAX
 MM MAX; max = x
-SKIP GD /000
-LV /001
+SKIP LV /001
 AD I
-MM I; i = i-1
+MM I; i = i+1
 JP LOOP
 
 IMPRIME LD MAX
-ML MULT; olha apenas segundo digito
-SB COUT
-JN SOMA; dig - A < 0 ?
-LD MAX
-SB A ; -A + 10
-AD MULT
-MM MAX
-SOMA LD MAX; carrega soma
 AD BASE
 PD /100; print
 
