146fa5710a2b9f6887bcbb3626e0ab55e821ccb7
Augusto <augustopmello@gmail.com>
2026-03-24 17:19:30 -0300

    removing desnecessary comments

--- ep/mnem2op.c
+++ ep/mnem2op.c
@@ -32,7 +32,6 @@ int getNum(char* s) {
         return strtol(s, NULL, 16);
     }
     return -1;
-    // return NULL;
 }
 
 int isNum(char* s) {
@@ -49,7 +48,6 @@ int isNum(char* s) {
 int main(int argc, char* argv []){
     int end =0;
     char* token; // string
-    // char ** newToken= malloc(10* sizeof(char* ));
     char * inst;
 
     char * simbolos[200];
@@ -71,14 +69,10 @@ int main(int argc, char* argv []){
         token = strtok(buffer, " ");
         inst = getInstruction(token);
         if(inst == NULL && !(token[0]=='\n') && !(token[0]=='K') ){
-            // if(strcmp("@",token) == 0 ){
             if('@'==token[0]){
                 token = strtok(NULL, " ");
                 end = getNum(token);
                 end-=2;
-                // if(end == NULL){
-
-                // }
             } else {
                 simbolos[i] = malloc(strlen(token) + 1);
                 strcpy(simbolos[i],token);
@@ -89,15 +83,12 @@ int main(int argc, char* argv []){
         end+=2;
     }
 
-    int n = i+1;// número de mnemonicos achados
-
     for(int j=0; j<i; j++){
     }
-    // for(int j=0; j<i; j++){
-    // }
 
     rewind(input);
     end=0;
+
     // segundo passo
     while(fgets(buffer, 100,input)){
         token = strtok(buffer, " ");
@@ -106,10 +97,8 @@ int main(int argc, char* argv []){
             if('@'==token[0]){
                 token = strtok(NULL, " ");
                 end = getNum(token);
-                // if(end == NULL){
                     
                 end-=2;
-                // }
             } else if ('K'==token[0] ){
                 token = strtok(NULL, " ");
                 int num = getNum(token);
@@ -139,31 +128,11 @@ int main(int argc, char* argv []){
                             }
                         }
                     } else {
-                    
                         op[4] = '\0';
-                        // op++;
                         fprintf(output,"%04X %s%03X\n", end, inst, getNum(op));
                     }
                 
                 }
-                // if(!isNum(op)){ // basicamente quero que ele detecte se trata-se de mnemonico ou de absoluto
-                //     for(int j=0; j<i; j++){
-                //         if(strcmp(token, simbolos[j])==0) {
-                //             fprintf(output, "%04X %s%03X\n", end, inst, enderecos[j]);
-                //             break;
-                //         }
-                //     }
-                // } else {
-                    
-                //     op[3] = '\0';
-                //     fprintf(output,"%04X %s%s\n", end, inst, op);
-                // }
-                
-                // simbolos[i] = token;
-                
-                // for(int j=0; j<i; j++){
-                // }
-
             }
 
         } else {
@@ -179,14 +148,12 @@ int main(int argc, char* argv []){
             } else {
 
                 op[4] = '\0';
-                // op++;
                 fprintf(output,"%04X %s%03X\n", end, inst, getNum(op));
             }
         
         }
 
         end+=2;
-        // free(newToken);
     }
 
 }
